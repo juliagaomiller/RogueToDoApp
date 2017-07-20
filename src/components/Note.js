f9e0a974269573a6ecbd30f7ab54b75567bd865a
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-07-20 12:15:07 -0400

    Created Note.js

---
+++ src/components/Note.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+
+});
